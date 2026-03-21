daf0dc5856d921e912a27ccdeb589cc70517172f
violettadudiak0210 <violettadudiak0210@gmail.com>
2026-03-21 22:03:42 +0000

    Fix updateNote function

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -39,7 +39,7 @@ export const deleteNote = async (req, res, next) => {
 export const updateNote = async (req, res, next) => {
   const { noteId } = req.params;
   const note = await Note.findByIdAndUpdate(noteId, req.body, {
-    new: true,
+    returnDocument: 'after',
     runValidators: true,
   });
 
