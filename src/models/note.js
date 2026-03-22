d48894a9519843843c35824a210932010292fd44
violettadudiak0210 <violettadudiak0210@gmail.com>
2026-03-22 13:43:41 +0000

    fix note.js

--- src/models/note.js
+++ src/models/note.js
@@ -18,6 +18,11 @@ const noteSchema = new Schema(
       enum: TAGS,
       default: 'Todo',
     },
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: 'User', // посилання на модель User
+      required: true,
+    },
   },
   {
     timestamps: true,
