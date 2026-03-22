0ee766a5058410703c6acf20275d8a5802d9331f
violettadudiak0210 <violettadudiak0210@gmail.com>
2026-03-22 13:13:13 +0000

    updateNote fix

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -52,7 +52,7 @@ export const deleteNote = async (req, res) => {
 export const updateNote = async (req, res) => {
   const { noteId } = req.params;
   const note = await Note.findOneAndUpdate({ _id: noteId }, req.body, {
-    new: true,
+    returnDocument: 'after',
   });
 
   if (!note) {
